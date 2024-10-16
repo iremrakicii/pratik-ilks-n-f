7bafb4c4feded9c3bd4263300eacaa0f861dc907
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-16 12:18:04 +0300

    Proje dosyası ekle.

---
+++ pratik-ilksınıf/Person.cs
@@ -0,0 +1,61 @@
+﻿namespace pratik_ilksınıf
+{
+    public class Person
+    {
+        private string name = string.Empty;
+        private string surname = string.Empty;
+        private DateTime birthDate;
+
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+
+            set
+            {
+                surname = value;
+            }
+        }
+        public DateTime BirthDate
+        {
+            get
+            {
+                return birthDate;
+            }
+
+            set
+            {
+                birthDate = value;
+            }
+        }
+
+        public void IsimYazdir()
+        {
+
+            Console.WriteLine($"Ad : {name} ");
+        }
+        public void SoyadYazdir()
+        {
+
+            Console.WriteLine($"Soyad : {surname} ");
+        }
+        public void DogumTarihi()
+        {
+            Console.WriteLine($"Doğum Tarihi  : {birthDate} ");
+        }
+    }
+}
---
+++ pratik-ilksınıf/Program.cs
@@ -0,0 +1,46 @@
+﻿using pratik_ilksınıf;
+
+class Program
+{
+    static void Main()
+    {
+        Person ogrenci = new Person();
+
+        Console.WriteLine("Öğrencimizin kişisel bilgileri:");
+        ogrenci.Name = "İrem";
+        ogrenci.Surname = "Rakıcı";
+        ogrenci.BirthDate = new DateTime(2000, 09, 12);
+
+        ogrenci.IsimYazdir();
+        ogrenci.SoyadYazdir();
+        ogrenci.DogumTarihi();
+
+        Console.WriteLine("--------------------------");
+
+        Person ogrenci2 = new Person();
+
+        Console.WriteLine("Öğrencimizin kişisel bilgileri:");
+        ogrenci2.Name = "Yasin Enes";
+        ogrenci2.Surname = "Şişik";
+        ogrenci2.BirthDate = new DateTime(2000, 03, 12);
+
+        ogrenci2.IsimYazdir();
+        ogrenci2.SoyadYazdir();
+        ogrenci2.DogumTarihi();
+
+        Console.WriteLine("--------------------------");
+
+        Person ogretmen = new Person();
+
+        Console.WriteLine("Öğretmenimizin kişisel bilgileri:");
+
+        ogretmen.Name = "Ahmet";
+        ogretmen.Surname = "Rakıcı";
+        ogretmen.BirthDate = new DateTime(1966, 09, 26);
+
+        ogretmen.IsimYazdir();
+        ogretmen.SoyadYazdir();
+        ogretmen.DogumTarihi();
+
+    }
+}
